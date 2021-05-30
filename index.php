087b39432b5a9ab77b0b39494e285c438138fb0e
Victor Feitosa <victsfpx@br.ibm.com>
2021-05-30 12:09:37 -0300

    deploy ftp

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FTP</title>
+    <title>GITHUB - FTP</title>
 </head>
 <body>
-    <h1><?php echo 'Hello World' ?></h1> 
+    <h1><?php echo 'Hello World - FTP' ?></h1> 
 </body>
 </html>
